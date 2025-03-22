4aa6b57f01c085533723e33f370de50a37e78d38
ganeshkanna_subramanian <ganeshkanna.subramanian@impigertech.com>
2025-03-22 12:53:36 +0530

    Added additional commands

--- serialpor.py
+++ serialpor.py
@@ -10,7 +10,7 @@ req = [0x2A, 0x80, 0x00, 0x03, 0x01, 0x03, 0x52, 0x45, 0x51, 0x81, 0x23]
 receipt_ack = [0x2A, 0x80, 0x00, 0x03, 0x05, 0x01, 0x41, 0x43, 0x4B, 0x81, 0x23]
 ready = [0x2A, 0x80, 0x00, 0x05, 0x05, 0x02, 0x52, 0x45, 0x41, 0x44, 0x59, 0x81, 0x23]
 current_mm = [0x2A, 0x80, 0x00, 0x02, 0x05, 0x03, 0x31, 0x31, 0x81, 0x23]
-current_passage = [0x2A, 0x80, 0x00, 0x01, 0x05, 0x03, 0x31, 0x81, 0x23]
+current_passage = [0x2A, 0x80, 0x00, 0x01, 0x05, 0x04, 0x31, 0x81, 0x23]
 process_complete = [0x2A, 0x80, 0x00, 0x08, 0x05, 0x05, 0x43, 0x4F, 0x4D, 0x50, 0x4C, 0x45, 0x54, 0x45, 0x81, 0x23]
 cancel_ack = [0x2A, 0x80, 0x00, 0x03, 0x05, 0x06, 0x41, 0x43, 0x4B, 0x81, 0x23]
 reset_ack = [0x2A, 0x80, 0x00, 0x03, 0x05, 0x07, 0x41, 0x43, 0x4B, 0x81, 0x23]
@@ -57,29 +57,29 @@ while 1:
             print('Sending machine ready ack')
             serialPort.write(serial.to_bytes(machineready_ack))
         elif (serialString.__contains__(b'\x05\x011')):
-            print('Sending receipt ack')
+            print('Sending recepp ack')
             serialPort.write(serial.to_bytes(receipt_ack))
             time.sleep(3)
             print('Sending ready')
             serialPort.write(serial.to_bytes(ready))
-            time.sleep(3)
-            print('Sending current mm')
-            serialPort.write(serial.to_bytes(current_mm))
+            # time.sleep(3)
+            # print('Sending current mm')
+            # serialPort.write(serial.to_bytes(current_mm))
             time.sleep(3)
             print('Sending current passage')
             serialPort.write(serial.to_bytes(current_passage))
-            time.sleep(3)
-            print('Sending process complete')
-            serialPort.write(serial.to_bytes(process_complete))
+            # time.sleep(3)
+            # print('Sending process complete')
+            # serialPort.write(serial.to_bytes(process_complete))
         elif (serialString.__contains__(b'CANCEL')):
             print('Sending cancel ack')
             serialPort.write(serial.to_bytes(cancel_ack))
         elif (serialString.__contains__(b'RESET')):
             print('Sending RESET ack')
             serialPort.write(serial.to_bytes(reset_ack))
-            time.sleep(3)
-            print('Sending reset complete')
-            serialPort.write(serial.to_bytes(reset_complete))
+            # time.sleep(3)
+            # print('Sending reset complete')
+            # serialPort.write(serial.to_bytes(reset_complete))
         elif (serialString.__contains__(b'\x02\x02')):
             print('Sending Exponential data ack')
             serialPort.write(serial.to_bytes(exponentaial_ack))
